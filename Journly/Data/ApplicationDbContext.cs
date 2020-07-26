fc08564137cbf24bfbf8d7a11f52c3113def3aea
Heidi Sprouse <hsprouse91@gmail.com>
2020-07-26 13:48:21 -0500

    set up ApplicationDbContext.cs

---
+++ Journly/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Journly.Models;
+
+namespace Journly.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<FlaggedWord> FlaggedWord { get; set; }
+        public DbSet<MoodType> MoodType { get; set; }
+        public DbSet<Post> Post { get; set; }
+        public DbSet<Therapist> Therapist { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserRelationship> UserRelationship { get; set; }
+        public DbSet<UserType> UserType { get; set; }
+
+    }
+}
+
