5268cfcb975000319e5fc8a1f1074058c73df6a5
Heidi Sprouse <hsprouse91@gmail.com>
2020-08-25 20:00:44 -0500

    created a model for Note

---
+++ Journly/Models/Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Journly.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int TherapistId { get; set; }
+
+        public User Therapist { get; set; }
+
+        [Required]
+        public int ClientId { get; set; }
+
+        public User Client { get; set; }
+
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
